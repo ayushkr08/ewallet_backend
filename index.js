bf22fd5d44c6914d809a2bf3bdca2afbacce9d8c
Ayush Kumar <ayushishan08@gmail.com>
2023-05-12 20:10:34 +0530

    added basic username and password

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-console.log("hi")
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 5001
+var userdb = {
 
+}
+
+
+
+app.use(bodyParser.json())
+app.post('/signup', (req, res) => {
+    let username = req.body.username
+    let password = req.body.password
+    userdb[username] = password
+    console.log(userdb)
+    res.send("signup successfull")
+})
+app.post('/login', (req, res) => {
+    let username = req.body.username
+    let password = req.body.password
+    res.json({ "username": username, "password": password })
+
+})
 app.get('/', function (req, res) {
     console.log("request received")
     res.send('ayush here')
